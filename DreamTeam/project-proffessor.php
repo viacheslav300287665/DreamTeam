b4adcce969d651113c72a60e645328ed1c99be63
Dexter <49821562+dextersharma03@users.noreply.github.com>
2020-04-10 15:27:05 -0700

    Added the Professor Page with forms

--- DreamTeam/project-proffessor.php
+++ DreamTeam/project-proffessor.php
@@ -14,22 +14,28 @@ require_once("inc/Utility/Page.class.php");
 //session_start();
 //We must get Instructor that user entered from the search Page!! Its just a basic example of how it's gonna work
 
-RatingDAO::initialize();
-$instructor = new Instructor();
-$instructor->setInstructorID(1);
-$instructor->setCourseID(1);
-$instructor->setFirstName("Rahim");
-$instructor->setLastName("Virani");
-$instructor->setEmail("rahimvirani@douglascollege.ca");
-$reviews = RatingDAO::getInstructorReviews($instructor);
-CourseDAO::initialize();
-$courses = CourseDAO::getInstructorCourse($instructor);
-var_dump($courses);
-var_dump($reviews);
-Page::header();
-Page::listProffesorReviews($instructor , $reviews, $courses);
-Page::footer();
+// RatingDAO::initialize();
+// $instructor = new Instructor();
+// $instructor->setInstructorID(1);
+// $instructor->setCourseID(1);
+// $instructor->setFirstName("Rahim");
+// $instructor->setLastName("Virani");
+// $instructor->setEmail("rahimvirani@douglascollege.ca");
+// $reviews = RatingDAO::getInstructorReviews($instructor);
+// CourseDAO::initialize();
+// $courses = CourseDAO::getInstructorCourse($instructor);
+// var_dump($courses);
+// var_dump($reviews);
+// Page::header();
+// Page::listProffesorReviews($instructor , $reviews, $courses);
+// Page::footer();
 //LoginManager::verifyLogin();
+
+Page::headerForProfessor();
+Page::searchFormProfessor();
+Page::reviewsSection();
+Page::ratingsForm();
+Page::footerforProfessor();
 
 
 
