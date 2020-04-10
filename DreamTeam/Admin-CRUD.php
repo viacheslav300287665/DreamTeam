3c2e47eee666f0a96fed7e0adfb6e4a11f043166
robin27sidhu <Robin27sidhu@gmail.com>
2020-04-10 12:05:18 -0700

    admin Course CRUD done

--- DreamTeam/Admin-CRUD.php
+++ DreamTeam/Admin-CRUD.php
@@ -30,10 +30,33 @@ if (!empty($_POST)) {
         
         //Send the section to the DAO to be created
         CourseDAO::createCourse($newCourse);
+    }else if ($_POST["action"] == "edit"){
+        //Assemble the Section to Insert
+         
+        $editCourse = new Course();
+        //Send the section to the DAO for insertion
+        $editCourse->setCourseID($_POST["courseid"]);
+        $editCourse->setCourseShortName($_POST["courseshortname"]);
+        $editCourse->setCourseLongName($_POST["courselongname"]);
+        
+        //Send the section to the DAO to be created
+        CourseDAO::updateCourse($editCourse);
+
     }
 
+}//If there was a delete that came in via GET
+if (isset($_GET["action"]) && $_GET["action"] == "delete")  {
+    //Call the DAO and delete the respecitve Section
+    CourseDAO::deleteCourse($_GET["id"]);
 }
-
+//If someone clicked Edit
+if (isset($_GET["action"]) && $_GET["action"] == "edit")  {
+    //Pull the section to Edit from the DAO
+    $courseToEdit = CourseDAO::getCourse($_GET["id"]);
+    
+    //Render the  Edit Section form with the section to edit and  alist of the courses.
+    Page::editCourseForm($courseToEdit);
+}
 //show
 Page::header();
 Page::listCourses(CourseDAO::getCourses());
