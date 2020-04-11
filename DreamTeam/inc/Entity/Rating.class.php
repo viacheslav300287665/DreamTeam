03414ea2208677ceca85a1f50d4c93b9be020b77
robin27sidhu <Robin27sidhu@gmail.com>
2020-04-10 17:41:48 -0700

    updated entities

--- DreamTeam/inc/Entity/Rating.class.php
+++ DreamTeam/inc/Entity/Rating.class.php
@@ -1,14 +1,22 @@
 <?php
 class Rating{
-// RatingID INT PRIMARY KEY AUTO_INCREMENT,
-// InstructorID INT NOT NULL,
-// Rating INT NOT NULL,
-// Review VARCHAR(255),
+    // RatingID INT PRIMARY KEY AUTO_INCREMENT,
+    // InstructorID INT NOT NULL,
+    // CourseID INT NOT NULL,
+    // StudentID INT NOT NULL,
+    // Date Date,
+    // Rating INT NOT NULL,
+    // Review VARCHAR(255),
 
 private $RatingID;
 private $InstructorID;
+private $CourseID;
+private $StudentID;
+private $Date;
 private $Rating;
 private $Review;
+
+
 
 //Setters
 public function setRatingID(int $ratingID){
@@ -16,6 +24,15 @@ public function setRatingID(int $ratingID){
 }
 public function setInstructorID(int $instructorID){
     $this->InstructorID=$instructorID;
+}
+public function setCourseID(int $courseID){
+    $this->CourseID=$courseID;
+}
+public function setStudentID(int $studentID){
+    $this->StudentID=$studentID;
+}
+public function setDate(string $date){
+    $this->Date=$date;
 }
 public function setRating(int $ratingID){
     $this->Rating=$rating;
@@ -30,6 +47,15 @@ public function getRatingID():int{
 }
 public function getInstructorID():int{
     return $this->InstructorID;
+}
+public function getCourseID():int{
+    return $this->CourseID;
+}
+public function getStudentID():int{
+    return $this->StudentID;
+}
+public function getDate(){
+    return $this->Date;
 }
 public function getRating() :int{
     return $this->Rating;
