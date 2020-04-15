9d34fd851559bfcc161045b0451173ebfdbba0fe
Dexter <49821562+dextersharma03@users.noreply.github.com>
2020-04-14 19:04:50 -0700

    commit

--- pro-dreamteam/inc/config.inc.php
+++ pro-dreamteam/inc/config.inc.php
@@ -1,15 +1,15 @@
 <?php
 //////////////
-$urlpeices = explode("/",$_SERVER['REQUEST_URI']);
-$api_url = "http://localhost";
+// $urlpeices = explode("/",$_SERVER['REQUEST_URI']);
+// $api_url = "http://localhost:8080";
 
-for ($i = 0; $i < count($urlpeices) -1; $i++)   {
-    $api_url .= "/".$urlpeices[$i];
-}
-$api_url .= "/RestAPI.php";
+// for ($i = 0; $i < count($urlpeices) -1; $i++)   {
+//     $api_url .= "/".$urlpeices[$i];
+// }
+// $api_url .= "/RestAPI.php";
 
 
-define('API_URL', $api_url);
+// define('API_URL', $api_url);
 
 
 
@@ -21,6 +21,6 @@ define("DB_PASS", "");
 define("DB_NAME", "csis3280project");  
 
 
-//define('API_URL','http://localhost/dreamteam/RestAPI.php')
+define('API_URL','http://localhost:8080/finalPro/RestAPI.php')
 
 ?>
